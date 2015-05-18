6018a629286fa84584a7ed586b04058e5d0b4157
Matthew J. Berger <pacoishere@gmail.com>
2015-05-17 17:35:06 -0700

    Linux port

--- Globals.h
+++ Globals.h
@@ -3,10 +3,10 @@
 
 #define PI 3.1415
 
-#include "SDL.h"
-#include "SDL_image.h"
-#include "SDL_ttf.h"
-#include "SDL_mixer.h"
+#include "SDL2/SDL.h"
+#include "SDL2/SDL_image.h"
+#include "SDL2/SDL_ttf.h"
+#include "SDL2/SDL_mixer.h"
 #include "CTexture.h"
 #include "CButton.h"
 #include <iostream>
